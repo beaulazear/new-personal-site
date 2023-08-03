b270e86b3ae57f1dbebecadbf960d0a4d180b6e6
Beau <beaulazear@gmail.com>
2023-08-03 19:23:32 -0400

    fixing routing issues

--- src/App.js
+++ src/App.js
@@ -10,12 +10,12 @@ function App() {
   return (
     <div>
       <PageNavLinks />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/animal_care" element={<AnimalCarePage />} />
-        <Route path="/resume" element={<Resume />} />
-        <Route path="/programming" element={<Programming />} />
-      </Routes>
+        <Routes>
+          <Route path="new-personal-site/" element={<Home />} />
+          <Route path="new-personal-site/animal_care" element={<AnimalCarePage />} />
+          <Route path="new-personal-site/resume" element={<Resume />} />
+          <Route path="new-personal-site/programming" element={<Programming />} />
+        </Routes>
     </div>
   );
 }
