904da6815be0fbaa6e284dc30e7426e8f0455bef
Beau Lazear <beaulazear@gmail.com>
2023-12-18 17:38:44 -0500

    changed homepage to be styled similarly too animal care and code pages

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,6 +16,26 @@ const welcomeMessageStyles = {
   textAlign: 'center',
 };
 
+const StyledIntroduction = styled.div`
+  max-width: 1000px;
+  margin: 0 auto;
+  padding: 20px;
+
+  @media (max-width: 800px) {
+    max-width: 600px;
+  }
+`;
+
+const StyledLink = styled(NavLink)`
+  color: #3498db;
+  text-decoration: underline;
+
+  @media (max-width: 800px) {
+    color: #3498db;
+    text-decoration: underline;
+  }
+`;
+
 const Heading = styled.h2`
   font-size: 2em;
   margin-top: 40px;
@@ -65,7 +85,27 @@ const Home = () => {
       <div style={imageWrapper}>
         <img src={BeauAndHornbill} alt="Beau with a dog named Ru" style={image} />
       </div>
-      <SubHeading>About Me</SubHeading>
+      <StyledIntroduction>
+        <Heading>Computer Programming</Heading>
+        <Paragraph>
+          Welcome to my programming portfolio! I'm a web developer proficient with JavaScript, Ruby on Rails, React, HTML, and CSS. With a background in the animal care industry, I've been combining my love for animals with my coding skills to create projects for my dog walking business.
+          <br /><br />
+          I'm still exploring the world of web development, and my portfolio showcases my journey as I learn and grow. From front-end to back-end, I enjoy building websites that are functional and visually appealing. Take a look at my projects to visualize my progress.
+          <br /><br />
+          Looking for help on your next project? Contact me with any inquiries, and we can disuss how I can help you out! Visit <StyledLink to="/resume">resume page</StyledLink> for contact info.
+        </Paragraph>
+      </StyledIntroduction>
+      <StyledIntroduction>
+        <Heading>Computer Programming</Heading>
+        <Paragraph>
+          Welcome to my programming portfolio! I'm a web developer proficient with JavaScript, Ruby on Rails, React, HTML, and CSS. With a background in the animal care industry, I've been combining my love for animals with my coding skills to create projects for my dog walking business.
+          <br /><br />
+          I'm still exploring the world of web development, and my portfolio showcases my journey as I learn and grow. From front-end to back-end, I enjoy building websites that are functional and visually appealing. Take a look at my projects to visualize my progress.
+          <br /><br />
+          Looking for help on your next project? Contact me with any inquiries, and we can disuss how I can help you out! Visit <StyledLink to="/resume">resume page</StyledLink> for contact info.
+        </Paragraph>
+      </StyledIntroduction>
+      {/* <SubHeading>About Me</SubHeading>
       <Paragraph> Hi, I'm Beau! I am based in Brooklyn NYC. I've had an eventful professional life involving multiple direct animal care positions, administrative & hospitality roles (within plant-based / cruelty-free establishments), and more. I've grown within these roles, and completed several educational programs / projects throughout my experience. I am currently maintaining my own business - providing exceptional animal care to clients across Brooklyn NYC. I am self-taught in basic computer programming, and I am a graduate from the Flatiron School's software development program. Proficiency in HTML, Javascript, React, CSS, Ruby & Rails, and more as I continue to learn.
       </Paragraph>
       <SubHeading>What can I do for you?</SubHeading>
@@ -76,7 +116,7 @@ const Home = () => {
         <br />
         <br />
         Visit <NavLink to="/resume">Resume</NavLink> page for contact info.
-      </Paragraph>
+      </Paragraph> */}
     </div>
   );
 };
